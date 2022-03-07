5e3508d511c765a0f9e73dc8655a7c0b86cb15ac
ahmichal <95378632+ahmichal@users.noreply.github.com>
2022-03-07 10:34:32 +0200

    "last name only"

--- Michal.java
+++ Michal.java
@@ -1,7 +1,7 @@
 public class Michal{
 	public static void main(String[] args){
 		for (int i=1;i<=10;i++){
-		System.out.println("Michal but without last name"+i);
+		System.out.println("aharoni"+i);
 		}
 	}
 }
